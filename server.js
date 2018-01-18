2f615603411b12cf4b91a85a06f35a92d3040444
Syed Umair <syed.yousuff@full.co>
2018-01-18 15:08:35 +0530

    Refactored Information logging

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ var server = http.createServer(function(req, response) {
   if (req && req.url.indexOf('/crashreporter') !== -1) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
-      console.log("JSON:", JSON.stringify({
-          fields: fields,
-          files: files
-      }));
+      console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("New Crash Reported::");
+      console.log("\tUserName: ",fields.userName);
+      console.log("\tEmail: ",fields.email);
+      console.log("\tCompany:",fields._companyName);
+      console.log("\tProductName: ",fields._productName);
+      console.log("\tVersion: ",fields._version);
+      console.log("\tElectron Engine Version: ",fields.ver);
+      console.log("\tMode: ",fields.mode);
+      console.log("\tOS: ",fields.os);
+      console.log("\tArchitecture: ",fields.platform);
+      console.log("\tCrashed Process Type: ",fields.process_type);            
+      console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     });
   }
 });
